90af07b2b4b099f809496a56972d06c37c28103e
willysvarela <willys.cunha@grupoicts.com.br>
2020-10-08 19:37:46 -0400

    added facebook pixel

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
+import ReactPixel from 'react-facebook-pixel';
 
 import App from './App';
 import './index.css';
+
+ReactPixel.init('257753242273552', null, {
+  autoConfig: true, // set pixel's autoConfig
+  debug: false, // enable logs
+});
+ReactPixel.pageView();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +19,6 @@ ReactDOM.render(
     </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
